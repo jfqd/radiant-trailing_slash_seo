d1c61bc2d90cf5bbbc8f4eccba7b5e5f3022d26f
jfqd <jfqd@blun.org>
2011-03-11 16:56:07 +0100

    fix github project path

--- trailing_slash_seo_extension.rb
+++ trailing_slash_seo_extension.rb
@@ -3,7 +3,7 @@ require_dependency "#{File.expand_path(File.dirname(__FILE__))}/lib/trailing_sla
 class TrailingSlashSeoExtension < Radiant::Extension
   version "1.0"
   description "Redirects Page urls without slash to version with slash to do some SEO"
-  url "http://github.com/mikz/radiant-trailing-slash-seo"
+  url "https://github.com/mikz/radiant-trailing_slash_seo"
     
   def activate
     Page.send :include, TrailingSlashSeo
